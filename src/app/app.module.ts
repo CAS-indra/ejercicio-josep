752543a6adf42e26a07fd8a8c81eb27dc03d12c5
JosepFerrusPellicer <jferrus@minsait.com>
2021-09-16 18:38:22 +0200

    ejercicio-josep

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TituloComponent } from './titulo/titulo.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { LayoutModule } from './layout/layout.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TituloComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/titulo/titulo.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-titulo',
+  templateUrl: './titulo.component.html',
+  styleUrls: ['./titulo.component.scss']
+})
+export class TituloComponent  {
+  public title = 'App ejercicio de Josep';
+
+}
